38380e1171586d81cd7a969219da879648f84102
Mykyta Tymchenko <mykyta.tymchenko@nixs.com>
2026-02-09 16:29:17 +0200

    test ci

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace GrpcDemoService
         /// <param name="args">The command-line arguments passed to the application.</param>
         public static void Main(string[] args)
         {
+            // test
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddGrpc().AddJsonTranscoding();
